047b55b60f52fbd1d5ad18fadae235810659eedb
xan <xan.xan@gmail.com>
2018-06-16 18:36:17 +0200

    javadoc for state machine class

--- src/main/kotlin/es/xan/servantv3/api/StateMachine.kt
+++ src/main/kotlin/es/xan/servantv3/api/StateMachine.kt
@@ -4,7 +4,14 @@ import io.vertx.core.json.JsonObject
 import io.vertx.core.AbstractVerticle
 import es.xan.servantv3.Events
 
-
+/**
+ * I love state machines!
+ *
+ * And as I love state machines I always try to transform complex functional workflows into simple state machines.
+ * For this reason, I have created these interface and two abstract clases. 
+ *
+ * TODO
+ */
 interface State {}
 
 class Transition<V : AbstractVerticle, S : State>(val predicate : (JsonObject) -> Boolean, val operation: (V) -> S)
