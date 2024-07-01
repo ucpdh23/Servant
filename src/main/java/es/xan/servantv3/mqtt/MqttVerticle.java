d8b74dd91f60e8b1f3b34986e8a9b72bb6e386fb
xan <xan.xan@gmail.com>
2024-07-01 21:48:22 +0200

    solvong issue with Puerta

--- src/main/java/es/xan/servantv3/mqtt/MqttVerticle.java
+++ src/main/java/es/xan/servantv3/mqtt/MqttVerticle.java
@@ -107,7 +107,7 @@ public class MqttVerticle extends AbstractServantVerticle {
 
                         if (message.topicName().startsWith("zigbee2mqtt/Puerta")) {
 			    LOGGER.info("Puerta [{}]", message.payload().toJsonObject().toString());
-			    Boolean contact = message.payload().toJsonObject().getString("contact");
+			    Boolean contact = message.payload().toJsonObject().getBoolean("contact");
                             //JsonObject object = MqttUtils.resolvePayload(message.payload().toJsonObject().getString("message"));
                             //Boolean contact = object.getBoolean("contact");
                             LOGGER.info("Puerta [{}]", contact);
