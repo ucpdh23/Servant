debea2156468dc8cbc505b6f0be8c651a807944e
xan <xan.xan@gmail.com>
2025-06-14 19:49:31 +0200

    Adding new outlet capabilities

--- src/main/kotlin/es/xan/servantv3/messages/Messages.kt
+++ src/main/kotlin/es/xan/servantv3/messages/Messages.kt
@@ -84,6 +84,7 @@ enum class DeviceSecurity {
 }
 
 data class Agent(var name: String)
+data class Chatbot(var user: String, var message: String)
 data class Executed(var message: String)
 
 data class Device(var user: String, var mac: String, var status: DeviceStatus, var security: DeviceSecurity, var firstTimestamp : Long, var lastTimestamp: Long)
